dc21c81dcc9df0f221787ee0e24ca3c6cd76a2d9
parzaech <yash.varshney1103@gmail.com>
2026-06-27 13:26:41 +0530

    firsty

---
+++ ttt.py
@@ -0,0 +1,86 @@
+import random
+import numpy as np
+
+#DEFINE PROBLEM
+#No. of states != 3^9
+#Only max X's = 5/4 and same with O's, therefore it nicely reduce the count(set)<3^9
+#Count(set)=around 5.5k
+
+class ticmactoe:
+    def __init__(self, starting_player = 1):
+        #initialised the starting move for 1 = X
+        self.starting_player = starting_player
+        self.board = np.zeros((3,3), dtype=int) #3x3 empty board
+        
+        self.current_player = starting_player #current chance at any state
+
+    def reset(self):
+        #for restarting the play and emptying the board
+        self.board = np.zeros((3,3), dtype=int)
+        self.current_player = self.starting_player
+
+        #using a flattened matrix - tuple for every state will be helpful for making a hashtable for aquiring a particular state from Q-table
+        return tuple(self.board.flatten())
+
+    def check_win(self):
+        check = np.concatenate([np.sum(self.board, axis = 0), np.sum(self.board, axis = 1)
+                                ,[np.trace(self.board), np.trace(np.fliplr(self.board))]]) #its a 1d array, np.sum throws a (1x3) and np.trace throws a scalar - needs to be converted
+        if np.any(check == 3):
+            return 1 # Player 1 wins
+        if np.any(check == -3):
+            return -1 # Player -1 wins
+        return 0
+        
+    def get_valid_action(self):
+        #to know the legal moves
+        return np.where(np.flatten(self.board) == 0)[0].tolist()
+
+    def step(self, action):
+        row = action//3
+        col = action%3
+
+
+
+
+        #reward
+              
+#COUNT
+#If we start with X, then at every step if its O's turn, x = o + 1 && if its X's turn, x = o
+#Vice versa
+
+#To get the number of states, its also possible to start dfs or bfs, start from 1Xs and then go deepdown
+
+
+#ACTION
+#0|1|2
+#3|4|5
+#6|7|8
+#scalar action = {0,1,2,3,4,5,6,7,8}
+#row = A//3 && col = A%3 ex. A = 5; row = 1 && col = 2
+#
+
+
+#State machine ---> X = 1, O = -1 and 'empty' = 0
+#because of this if a diagnol/row/column achieves the winning state, either it will 3 or -3
+
+
+#CHECK_WIN
+#np.sum(board,axis=1/0) col/rows
+#(edge-case-1)for diagonal, check the trace(matrix) = 3
+#(edge-case-2)for anti-diagonal, flip the matric L->R and calc tr(fliplr(matrix)) = 3
+
+# def check_win(board, player):
+#     check = 3 * player #-3,3
+
+#     if(np.sum(board, axis = 0) == check):
+#         return True
+#     elif(np.sum(board, axis = 1) == check):
+#         return True
+#     elif(np.trace(board) == check):
+#         return True
+#     elif(np.trace(np.fliplr(board)) == check):
+#         return True
+
+    #if np.any(all_sums == 3): # Player 1 wins
+    #if np.any(all_sums == -3): # Player -1 wins
+
